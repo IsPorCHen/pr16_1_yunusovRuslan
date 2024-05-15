78b2c17b39544387ff5f9632134af186ad10f47e
IsPorCHen <105991525+IsPorCHen@users.noreply.github.com>
2024-05-15 14:11:36 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace prakt16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                char[] separators = new char[] { ' ', '.', ':', '…' };
+                string text = "";
+
+                using (StreamReader rd = File.OpenText("file.txt"))
+                {
+                    text += rd.ReadLine();
+                }
+
+                string[] proposal = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                string inputText = "";
+                while (inputText == "")
+                {
+                    Console.Write("Введите слово: ");
+                    inputText = Console.ReadLine();
+                }
+
+                int countWords = (from word in proposal
+                            where word.ToLower() == inputText.ToLower()
+                            select word).Count();
+
+                Console.WriteLine($"были найдены {countWords} вхождения (ий) " +
+                    $"поискового запроса '{inputText}'");
+            }
+            catch
+            {
+                Console.WriteLine("ошибка неверного формата");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ zad2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace zad2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label2.Visible = textBox1.Text.IndexOf('/') == -1 ? true : false ;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.IndexOf('/') != -1)
+            {
+                string[] array = textBox1.Text.Split('/');  
+
+                var countNumbers = (from word in textBox1.Text
+                                    where char.IsDigit(word) 
+                                    select word).Count();
+
+                var charsToSlash = array.TakeWhile(s => s != "/");
+
+                var charsAfterSlash = array.SkipWhile(s => s != "/").Skip(1)
+                      .Select(s => s.ToUpper());
+
+
+                using (StreamWriter wr = File.AppendText("file.txt"))
+                {
+                    string result = "";
+                    result += $"{countNumbers} ";
+
+                    foreach (var chars in charsToSlash)
+                    {
+                        result += $"{chars} ";
+                    }
+
+                    foreach (var chars in charsAfterSlash)
+                    {
+                        result += $"{chars}";
+                    }
+
+                    wr.WriteLine(result);
+                }
+            }
+        }
+    }
+}
